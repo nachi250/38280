a7e07832ccf0888a733d9d6e0354d87177203f72
ignacio soria <soria.ign@gmail.com>
2022-09-09 14:58:08 -0300

    Detalle producto desde firesbase

--- src/Components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Spinner from 'react-bootstrap/Spinner';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../Services/firebase/firebase'
 
 const ItemDetailContainer = ()=> {
 
@@ -11,20 +13,34 @@ const ItemDetailContainer = ()=> {
 
     const [items, setItems] = useState([])
     const [item, setItem] = useState({})
+    const [products, setProducts] = useState([])
+    const [product, setProduct] = useState({})
 
     useEffect(()=>{
         const items = getItems()
         items.then(items => {
             setItems(items)
-            setItem(items.find(item => item.id == itemId))
+            setItem(items.find(item => item.id === itemId))
         })
     }, [itemId]);
+
+    useEffect(()=> {
+        getDocs(collection(db, 'items')).then((snapshot) => {
+        const items = snapshot.docs.map(doc => {
+            return {id: doc.id, ...doc.data()}
+        })
+        console.log('item: ',items.find(product => product.id === itemId))
+        setProduct(items.find(product => product.id === itemId))
+
+    })
+
+    },[itemId])
 
 
     return (
         <>
         {items.length > 0 ?
-            <ItemDetail key={itemId} item={item} itemId={itemId}/>
+            <ItemDetail key={itemId} item={product} itemId={itemId}/>
             :
             <Spinner animation="border" role="status" className="m-5">
             <span className="visually-hidden">Loading...</span>
